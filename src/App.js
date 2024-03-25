b5d0f85de8e58e1e7a515508ce582e8eb8804e1e
Elufere Innocent <eluferedonatus@gmail.com>
2024-03-25 11:11:57 +0100

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
+// import Home from './pages/home/Home';
+// import Watch from './pages/watch/Watch';
+
+
+function App() {
+  return (
+    <div className="app">
+        {/* <Home /> */}
+        {/* <Watch /> */}
+        {/* <Register /> */}
+        <Login />
+    </div>
+  );
+}
+
+export default App;
